388775fa54cb22341fefebe0a714583c839124bf
imants miķelsons <imantsmikelsons1980@gmail.com>
2022-04-20 22:23:34 +0300

    Firs task done

--- HomeWork3/HomeWork3/Program.cs
+++ HomeWork3/HomeWork3/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using HomeWork3;
+
+Telefons telefons = new Telefons();
+
+Console.WriteLine("Enter size mark model:");
+string mark = Console.ReadLine();
+string size = Console.ReadLine();
+string model = Console.ReadLine();
+
+
+telefons.Mark = mark;
+telefons.Size = size;
+telefons.Model = model;
+
+Console.WriteLine($"Apsveicu jaunais telefons ir {telefons.Mark}:");
+
+
+
+
+
---
+++ HomeWork3/HomeWork3/Telefons.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork3
+{
+    public class Telefons
+
+    {
+        public string Size { get; set; }
+
+        public string Model { get; set; }
+
+        public string Mark { get; set; }
+
+        public void Call()
+        {
+        }
+        public void SMS()
+        {
+        }
+
+    }
+}
